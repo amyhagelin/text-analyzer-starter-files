8270ccd9e8c2f90c3cc7260482b4353141158ff3
amyhagelin <ahagelin@gmail.com>
2016-12-20 19:59:33 -0800

    final version

--- app.js
+++ app.js
@@ -1,46 +1,69 @@
 
-
-var text = //input from the form - how to do that?
-var textArray = text.split(/[ ,!.";:-?]+/);
-
-
-// total word count -
-function totalWordCount(textArray) {
-return textArray.length;
+// total word count
+function totalWordCount(text) {
+	var textArray = text.split(/[ ,!.";:-?]+/).filter(Boolean);
+	return textArray.length;
 }
 
 // unique word count
-function uniqueWordCount(textArray) {
-	var uniqueWords = {}
-	for (var i = 0; i < textArray.length; i++) {
+function wf(textArray) {
+  var uniqueWords = {}
+  for (var i = 0; i < textArray.length; i++) {
 		var key = textArray[i];
-		if (uniqueWords[key] < 1) {
-			uniqueWords[key] = 1;
-		}
+		if (uniqueWords[key] >= 1) {
+			uniqueWords[key]++;
+		} else {
+          uniqueWords[key] = 1;
+        }
 	}
-	return = Object.keys(uniqueWords).length; // should I count with a loop instead?
+  return uniqueWords;
+}
+
+function uniqueWordCount(text) {
+	var textArray = text.split(/[ ,!.";:-?]+/).filter(Boolean); 
+	return Object.keys(wf(textArray)).length; // should I count with a loop instead?
 }
 
 // average word length
-function avgWordLength(textArray) {
-	var counter = 0;
+function avgWordLength(text) {
+  var textArray = text.split(/[ ,!.";:-?]+/).filter(Boolean); 
 	var sumWordLengths = 0;
-	for (var i = 0; i < textArray.length; i++) {
-		sumWordLengths = sumWordLengths + textArray[i].length;
-		counter = counter + 1;
-	}
-	return sumWordLengths / counter;
+  textArray.forEach(function (word) {
+    sumWordLengths += word.length;
+  });
+	return (sumWordLengths / textArray.length).toFixed(2);
 }
 
 // average sentence length - split at .?!
 function avgSentenceLength(text) {
-	var sentenceArray = text.split(/[!.?]+/);
-	var counter = 0;
-	var sumSentenceLengths = 0;
-	for (var i = 0; i < sentenceArray.length; i++) {
-		sumSentenceLengths = sumSentenceLengths + sentenceArray[i].length;
-		counter = counter + 1;
-	}
-	return sumSentenceLengths / counter;
+  var sentenceArray = text.split(/[!.?]+/).filter(Boolean);
+  var numOfSentences = sentenceArray.length;
+  var sum = 0;
+  sentenceArray.forEach(function (sentence) {
+    sum += totalWordCount(sentence);
+  });
+  
+  return (sum / numOfSentences).toFixed(2);
 }
+
+// jQuery
+$("button.analyze").click(function () {
+	var text = $("#user-text").val();
+	// now you have the text you
+	var wc = totalWordCount(text);
+	var uwc = uniqueWordCount(text);
+	var awl = avgWordLength(text);
+	var asl = avgSentenceLength(text);
+	// do the rest here
+
+
+
+	// return results
+	// injecting the results in the DOM
+	$("dl.hidden").removeClass("hidden");
+	$("#wc").html(wc);
+	$("#uwc").html(uwc);
+	$("#awl").html(awl);
+	$("#asl").html(asl);
+})
 
